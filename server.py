083c4bce068be965ef028bcef2fbccb24c4d3c20
George Zhang <geetransit@gmail.com>
2025-10-04 23:35:32 -0400

    Set up API to call Gemini

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import csv
+import os
 import random
 import typing
 
 import fastapi
 import pydantic
+import google.genai
 
 import kmeans
 
@@ -12,7 +14,27 @@ with open("worldcities.csv", encoding="utf-8", newline="") as file:
     for row in csv.DictReader(file):
         cities[row["city_ascii"]] = (float(row["lat"]), float(row["lng"]))
 
+# Gemini connection
+gemini = google.genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+
 app = fastapi.FastAPI()
+
+class _AnalyzeLocationInput(pydantic.BaseModel):
+    name: str
+    prompt: typing.Optional[str] = None
+class _AnalyzeLocationOutput(pydantic.BaseModel):
+    text: str
+@app.post("/api/analyze-location")
+async def _analyze_location(options: _AnalyzeLocationInput) -> _AnalyzeLocationOutput:
+    prompt = options.prompt
+    if prompt is None:
+        prompt = f"I am building a data center at {options.name}. What are some regulations I might need to overcome?"
+    response = gemini.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=prompt,
+    )
+    return _AnalyzeLocationOutput(text=response.text)
+
 
 class _KMeansInput(pydantic.BaseModel):
     locations: list[tuple[float, float]]
