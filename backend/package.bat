b113e0a043dfc8c60a5d7b96ffa7f20ca2de6af5
George Zhang <geetransit@gmail.com>
2025-10-05 07:40:23 -0400

    Fix missing cloudflare.json on serverless

--- backend/package.bat
+++ backend/package.bat
@@ -1,5 +1,6 @@
 mkdir func
 pip install -t func --platform manylinux2014_x86_64 --only-binary :all: mangum -r requirements.in
+copy cloudflare.json func
 copy worldcities.csv func
 copy kmeans.py func
 copy server.py func
--- backend/package.sh
+++ backend/package.sh
@@ -2,6 +2,7 @@
 # See https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-create-dependencies
 mkdir func
 pip3 install -t func --platform manylinux2014_x86_64 --only-binary :all: mangum -r requirements.in
+cp cloudflare.json func
 cp worldcities.csv func
 cp kmeans.py func
 cp server.py func
